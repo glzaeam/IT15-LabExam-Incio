ea3c9c1d8a0a5cd4f3eee261a83e1a1c77d7b264
Glaiza Mae Incio <g.incio.543622@ummindanao.edu.ph>
2026-04-23 23:27:30 +0800

    Update layout design and fix connection string

--- obj/Debug/net10.0/TechCoreSolutions.AssemblyInfo.cs
+++ obj/Debug/net10.0/TechCoreSolutions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechCoreSolutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+093473a072fcd784aeecde6a120cd3cd937e55f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechCoreSolutions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechCoreSolutions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
